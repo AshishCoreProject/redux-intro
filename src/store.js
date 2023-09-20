65ff1af6fdf4c4a91b1fa2a61a72a1179562d2d5
AshishCoreProject <ashishbarthwal1799@gmail.com>
2023-09-20 15:57:13 +0530

    Working with Action Creator

--- src/store.js
+++ src/store.js
@@ -1,3 +1,6 @@
+//1.creating a store
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -8,12 +11,17 @@ function reducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
-    case "account/withraw":
+    case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
       if (state.loan > 0) return state;
       //LATER
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case "account/payLoan":
       return {
         ...state,
@@ -26,3 +34,33 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+//CREATING a ACTION CREATOR
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return { type: "account/requestLoan", payload: { amount, purpose } };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+console.log(store.getState());
+
+store.dispatch(withdraw(200));
+console.log(store.getState());
+
+store.dispatch(requestLoan(4000, "buy a video game"));
+console.log(store.getState());
+
+store.dispatch(payLoan());
+console.log(store.getState());
