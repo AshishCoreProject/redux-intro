1f515edced74f749daced015765dd03dbdcd5b1d
AshishCoreProject <ashishbarthwal1799@gmail.com>
2023-09-21 11:34:33 +0530

    Connecting our Redux App with React

---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,41 @@
+//1.creating a store
+
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+export default function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+
+//CREATING a ACTION CREATOR
+
+export function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+export function updateName(fullName) {
+  return { type: "customer/updateName", payload: fullName };
+}
--- src/store.js
+++ src/store.js
@@ -1,66 +1,12 @@
-//1.creating a store
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
 
-const initialState = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: "",
-};
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
 
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      //LATER
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
+const store = createStore(rootReducer);
 
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
-
-//CREATING a ACTION CREATOR
-function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
-}
-function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-
-function requestLoan(amount, purpose) {
-  return { type: "account/requestLoan", payload: { amount, purpose } };
-}
-
-function payLoan() {
-  return { type: "account/payLoan" };
-}
-
-store.dispatch(deposit(500));
-console.log(store.getState());
-
-store.dispatch(withdraw(200));
-console.log(store.getState());
-
-store.dispatch(requestLoan(4000, "buy a video game"));
-console.log(store.getState());
-
-store.dispatch(payLoan());
-console.log(store.getState());
+export default store;
